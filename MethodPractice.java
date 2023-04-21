048d1d663d476c83c34c3269ae8b5903716de850
富永 <tominaga.ryu.2023.kensyu@gmail.com>
2023-04-21 12:29:07 +0900

    javakadai3

--- MethodPractice.java
+++ MethodPractice.java
@@ -1,23 +1,30 @@
 package kadai3;
 
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Enumeration;
 
 public class MethodPractice {
     public static void main(String[] args) {
         //うるう年判定
-        int year = 2024;
+        int year = 2001;
         boolean isLeap = isLeapYear(year);
-        System.out.println(year + "年はうるう年か？：" + isLeap);
+        if(isLeap == true){
+            System.out.println(year + "年はうるう年です");
+        }else{
+            System.out.println(year + "はうるう年ではありません");
+        }
         //合計値
-        int num1 = 10;
+        int num1 = 100;
         int sum1 = getSum(num1);
         System.out.println(num1 + "の合計値は？：" + sum1);
         //素数判定
         int num2 = 2;
         boolean prime = isPrime(num2);
-        System.out.println(num2 + "は素数か？：" + prime);
+        if(prime == true){
+            System.out.println(num2 + "　は素数です");
+        }else{
+            System.out.println(num2 + "　は素数ではありません");
+        }
+
         //階乗
         int num3 = 5;
         var fact = favtorial(num3);
@@ -29,21 +36,25 @@ public class MethodPractice {
         //回文
         String s = "level";
         var palind = ispalindrome(s);
-        System.out.println(s + "は回文か？：" + palind);
+        if(palind){
+            System.out.println(s + " は回文です。");
+        }else{
+            System.out.println(s + " は回文ではありません");
+        }
         //年齢取得
-        int years = 2002;
-        int month = 10;
+        int years = 2001;
+        int month = 4;
         int day = 25;
-           //Calendarメソッドで返されたageをそれぞれprintlnに入れる
-        int age = getAge(years,month,day);
-        System.out.println("生年月日は " + years + "　年 " + month + " 月 " + day + " 日で、現在の年齢は " + age + "です。");
+          //Calendarメソッドで返されたageをそれぞれprintlnに入れる
+        int age = getAge(years, month, day);
+        System.out.println("生年月日は" + years + "年" + month + "月" + day + "日で、現在の年齢は" + age + "歳です。");
         //配列のソート
-        int[] scores = {29,42,52,12,5,45,67,1};
+        int[] scores = {29, 42, 52, 12, 5, 45, 67, 1};
         sortArray(scores);
         //二分探索
-//        int[] index = {13,11,42,36,71,29,38,23};
-//        sortArray(index);
-//        getIndex(index,36,0,index.length);
+        int[] index = {13, 11, 42, 36, 71, 29, 38, 23};
+        sortArray(index);
+        getIndex(index, 36, 0, index.length);
 
     }
 
@@ -68,7 +79,7 @@ public class MethodPractice {
     public static int getSum(int num) {
         //合計値を計算する処理
         int sum = 0;
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= num; i++) {
             sum += i;
         }
         return sum;
@@ -116,7 +127,8 @@ public class MethodPractice {
         }
         return true;
     }
-    public static int getAge(int year, int month, int day){
+
+    public static int getAge(int year, int month, int day) {
         //CalendarクラスでgetInstanceメソッドを呼び、オブジェクトを作成している、
         Calendar time = Calendar.getInstance();
         //変数「nowyear」に「time.get(Calendar.YEAR)」で取得した”年”を代入する。
@@ -128,26 +140,27 @@ public class MethodPractice {
         //ageに取得した”年”から引数で渡された”年”を引いた値を代入する。
         int age = nowyear - year;
         //nowmanthに代入した値がmainメソッドで入力したmonth値より小さい比較
-        if(nowmonth < month) {
+        if (nowmonth < month) {
             //小さい場合、ageを-1する
             age--;
-        //上のif文がfalseの場合
-        //nowmonthとmonthが等しい　かつ　nowdayが入力したdayより小さいかを比較
-        }else if(nowmonth == month && nowday < day){
+            //上のif文がfalseの場合
+            //nowmonthとmonthが等しい　かつ　nowdayが入力したdayより小さいかを比較
+        } else if (nowmonth == month && nowday < day) {
             //falseの場合ageから-1する
             age--;
         }
         //結果として出た値(age)を返す
         return age;
     }
+
     public static void sortArray(int[] array) {
         int[] scores = array;
 
-        for(int i = 0; i < scores.length - 1; i++) {
-            for(int j = scores.length - 1; j > 0 + i; j--) {
-                if(scores[j-1] > scores[j]) {
-                    int tmp = scores[j-1];
-                    scores[j-1] = scores[j];
+        for (int i = 0; i < scores.length - 1; i++) {
+            for (int j = scores.length - 1; j > 0 + i; j--) {
+                if (scores[j - 1] > scores[j]) {
+                    int tmp = scores[j - 1];
+                    scores[j - 1] = scores[j];
                     scores[j] = tmp;
                 }
             }
@@ -156,28 +169,24 @@ public class MethodPractice {
         for (int i = 0; i < scores.length; i++) {
             System.out.print(scores[i] + " ");
         }
+        System.out.println();
     }
 
-//    public static String getIndex(int[] index, int middle,int start,int end){
-//        int[] scores = index;
-//        var mdl = (start + end) / 2;
-//
-//        if(scores[mdl] == middle){
-//            middle = scores[mdl];
-//            System.out.println(middle + "は" + mdl + "番目です");
-//            return mdl;
-//        }
-//
-//        return mdl;
+    public static void getIndex(int[] index, int middle, int start, int end) {
+        int[] scores = index;
+        var mdl = (start + end) / 2;
 
-
-
-
-
-
-
-
+        if (scores[mdl] == middle) {
+            middle = scores[mdl];
+            System.out.println(middle + "は" + (mdl + 1) + "番目です");
+        } else if (scores[mdl] < middle) {
+            start = mdl + 1;
+        }else{
+            end = mdl - 1;
+        }
     }
+}
+
 
 
 
